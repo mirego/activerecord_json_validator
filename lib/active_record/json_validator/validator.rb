3858d7cbca3702c6cbf9f1d9e65f0e6dbb64f203
Rémi Prévost <remi@exomel.com>
2013-11-18 13:48:23 -0500

    Catch JSON load errors

--- lib/active_record/json_validator/validator.rb
+++ lib/active_record/json_validator/validator.rb
@@ -2,15 +2,35 @@ class JsonValidator < ActiveModel::EachValidator
   def initialize(options)
     options.reverse_merge!(message: :invalid_json)
     options.reverse_merge!(schema: nil)
+    @attributes = options[:attributes]
 
     super
   end
 
+  # Redefine the setter method for the attributes, since we want to
+  # catch any MultiJson::LoadError errors.
+  def setup(model)
+    @attributes.each do |attribute|
+      model.class_eval <<-RUBY, __FILE__, __LINE__ + 1
+        define_method "#{attribute}=" do |args|
+          begin
+            @_#{attribute}_sane_json = true
+            super(args)
+          rescue MultiJson::LoadError
+            @_#{attribute}_sane_json = false
+            super({})
+          end
+        end
+      RUBY
+    end
+  end
+
+  # Validate the JSON value with a JSON schema path or String
   def validate_each(record, attribute, value)
     json_value = JSON.dump(value)
     errors = ::JSON::Validator.fully_validate(options.fetch(:schema), json_value)
 
-    if errors.any?
+    if errors.any? || instance_variable_get(:"@_#{attribute}_sane_json") == false
       record.errors.add(attribute, options.fetch(:message), value: value)
     end
   end
