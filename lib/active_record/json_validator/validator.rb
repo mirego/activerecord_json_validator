b198fcc00f91ab1c35777b972960a0848ed5fc54
Briam Santiago <bm.santiago14@gmail.com>
2015-01-23 05:19:59 -0400

    Add dynamic schemas support.

--- lib/active_record/json_validator/validator.rb
+++ lib/active_record/json_validator/validator.rb
@@ -1,7 +1,18 @@
 class JsonValidator < ActiveModel::EachValidator
+
+  class << self
+    attr_writer :schema_method
+
+    def schema_method
+      @schema_method || "_json_schema"
+    end
+  end
+  
+
   def initialize(options)
-    options.reverse_merge!(message: :invalid_json)
-    options.reverse_merge!(schema: nil)
+    @options = options
+    @options.reverse_merge!(message: :invalid_json)
+    @options.reverse_merge!(schema: nil)
     @attributes = options[:attributes]
 
     super
@@ -22,20 +33,30 @@ class JsonValidator < ActiveModel::EachValidator
 
   # Validate the JSON value with a JSON schema path or String
   def validate_each(record, attribute, value)
+    @record = record
+
     begin
       json_value = JSON.dump(value)
     rescue JSON::GeneratorError
       json_value = ''
     end
 
-    errors = ::JSON::Validator.fully_validate(options.fetch(:schema), json_value)
+    errors = ::JSON::Validator.fully_validate(schema, json_value)
 
     if errors.any? || record.send(:"#{attribute}_invalid_json").present?
-      record.errors.add(attribute, options.fetch(:message), value: value)
+      record.errors.add(attribute, @options.fetch(:message), value: value)
     end
   end
 
 protected
+
+  def schema
+    if @record.respond_to? self.class.schema_method
+      @record.send self.class.schema_method
+    else
+      @options.fetch(:schema)
+    end
+  end
 
   # Redefine the setter method for the attributes, since we want to
   # catch any MultiJson::LoadError errors.
