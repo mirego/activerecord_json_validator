3d30fd7e20de37b86697f9bf9dc2746dcd0010f0
Rémi Prévost <remi@exomel.com>
2017-02-08 08:11:13 -0500

    Disable BlockLength rule in JsonValidator spec

--- spec/json_validator_spec.rb
+++ spec/json_validator_spec.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Metrics/BlockLength
 require 'spec_helper'
 
 describe JsonValidator do
@@ -185,3 +186,4 @@ describe JsonValidator do
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
