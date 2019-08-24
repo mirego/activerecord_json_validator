0b22f681183a7905591857b0ded75868c0ab60b9
Yosi Attias <yosy101@gmail.com>
2019-08-24 16:08:55 +0300

    Add check for migration class to support rails 4 to 6

--- spec/support/macros/database_macros.rb
+++ spec/support/macros/database_macros.rb
@@ -1,8 +1,14 @@
 module DatabaseMacros
   # Run migrations in the test database
   def run_migration(&block)
+    migration_class = if ActiveRecord::Migration.respond_to?(:[])
+                        ActiveRecord::Migration[4.2]
+                      else
+                        ActiveRecord::Migration
+                      end
+
     # Create a new migration class
-    klass = Class.new(ActiveRecord::Migration[4.2])
+    klass = Class.new(migration_class)
 
     # Create a new `up` that executes the argument
     klass.send(:define_method, :up) { instance_exec(&block) }
